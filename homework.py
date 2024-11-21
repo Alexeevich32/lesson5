9b87a3c5d128d50441d7e9937e787952ad977fa9
Admin <kozevnikovartem320@gmail.com>
2024-11-21 20:23:43 +0300

    21/11

---
+++ homework.py
@@ -0,0 +1,32 @@
+n= {5, 3, 3, 5, 4}
+number_grad =sum(n)
+quantity_grad =len(n)
+gpa = number_grad / quantity_grad
+print('gpa:',gpa)
+a = {2, 2, 2, 3}
+number_grad = sum(a)
+quantity_grad =len(a)
+gpa = number_grad / quantity_grad
+print('gpa:', gpa)
+b ={4, 5, 5, 2}
+number_grad = sum(b)
+quantity_grad =len(b)
+gpa = number_grad / quantity_grad
+print('gpa:', gpa)
+c ={4, 4, 3}
+number_grad = sum(c)
+quantity_grad =len(c)
+gpa = number_grad / quantity_grad
+print('gpa:', gpa)
+d ={5, 5, 5, 4, 5}
+number_grad = sum(d)
+quantity_grad =len(d)
+gpa = number_grad / quantity_grad
+print('gpa:', gpa)
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+print(sorted(students))
+school_magazine={'Aaron': 4.0 ,'Bilbo': 2.5 ,'Johnny' :3.6666666666666665 ,'Khendrik': 3.5 ,'Steve': 4.5}
+print(school_magazine)
+
+
+
